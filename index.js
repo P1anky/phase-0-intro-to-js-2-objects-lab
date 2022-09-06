3c4adc336fda8bfa4dbdbdc2f72bd79c8d0c56a6
Rob Sanders Hewett <robsh15@gmail.com>
2022-09-06 14:41:51 +0100

    commit tree 0564131d48a326bf300bc6a27cd81e799d0414f7

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name:"x",
+    streetAdress:"y"
+};
+function updateEmployeeWithKeyAndValue(employee,key,object){
+    return {
+        ...employee,
+        [key]:object,
+    };
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value;
+    return employee;
+};
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+};
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+};
